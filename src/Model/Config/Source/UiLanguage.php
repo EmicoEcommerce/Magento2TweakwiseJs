3a50c4c6bebfd6f763f387ccdf39e6ebaa4f259d
ahuininga-orisha <>
2026-03-27 15:49:32 +0100

    feat: add lang code

---
+++ src/Model/Config/Source/UiLanguage.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tweakwise\TweakwiseJs\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class UiLanguage implements OptionSourceInterface
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options = [
+            [
+                'label' => __('Default language'),
+                'value' => ''
+            ],
+            ['value' => 'en', 'label' => __('English')],
+            ['value' => 'nl', 'label' => __('Dutch')],
+            ['value' => 'de', 'label' => __('German')],
+            ['value' => 'fr', 'label' => __('French')],
+            ['value' => 'es', 'label' => __('Spanish')],
+            ['value' => 'it', 'label' => __('Italian')],
+        ];
+
+        return $options;
+    }
+}
