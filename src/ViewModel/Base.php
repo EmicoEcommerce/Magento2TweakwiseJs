da84134c35a1e3cb1755699512e7c6be2537c637
ahuininga-orisha <>
2026-05-22 15:50:24 +0200

    fix: implement functions initialisation for lister pages

--- src/ViewModel/Base.php
+++ src/ViewModel/Base.php
@@ -45,6 +45,14 @@ class Base implements ArgumentInterface
     /**
      * @return bool
      */
+    public function isAutoInitEnabled(): bool
+    {
+        return $this->config->isAutoInitEnabled();
+    }
+
+    /**
+     * @return bool
+     */
     public function isEventsEnabled(): bool
     {
         return $this->config->isEventsEnabled();
