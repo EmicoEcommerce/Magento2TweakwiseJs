2046c6e7f5695f57e511fe48373907dafe440895
ahuininga-orisha <ahuininga-orisha@users.noreply.github.com>
2026-06-11 12:56:04 +0200

    Fix autloading tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright Youwe. All rights reserved.
+ * https://www.youweagency.com
+ */
+
+declare(strict_types=1);
+
+spl_autoload_register(static function (string $class): void {
+    $prefix = 'Tweakwise\\Test\\';
+    $baseDir = __DIR__ . '/';
+
+    if (strncmp($prefix, $class, strlen($prefix)) !== 0) {
+        return;
+    }
+
+    $file = $baseDir . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
